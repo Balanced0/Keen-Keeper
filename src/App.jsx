6ee114128c8ace282d0fd50eb80d4f682434f2f7
balanced0 <alvichowdhury013@gmail.com>
2026-04-14 14:17:07 +0600

    implemented react router and created some routing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Outlet } from "react-router";
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
     </>
   )
 }
---
+++ src/components/routes/Router.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Homepage from "../homepage/Homepage";
+import App from "../../App";
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        index: true,
+        element: <Homepage></Homepage>,
+      },
+      {
+        path: "*",
+        element: <h1>Error 404</h1>,
+      },
+    ],
+  },
+]);
+
+export default Router;
