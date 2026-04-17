e30bdce361b7400f9de8a92bc8da624e3230468e
balanced0 <alvichowdhury013@gmail.com>
2026-04-17 23:56:42 +0600

    Fixed the loader problem in the homepage

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import Banner from '../banner/Banner';
-import { useLoaderData, useNavigation } from "react-router";
-import FriendsList from '../friendsList/FriendsList';
+import React, { useEffect, useState } from 'react';
+import Banner from "../banner/Banner";
+import { useLoaderData } from "react-router";
+import FriendsList from "../friendsList/FriendsList";
 
 const Homepage = () => {
-    const friendsData = useLoaderData();
-    const navigation = useNavigation();
-    if(navigation.state === "loading"){
-        return (<div className="flex justify-center items-center h-[60vh]"> <span className="loading loading-dots loading-xl"></span></div>);
-    }
+  const data = useLoaderData();
+  const [friendsData, setFriendsData] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      setFriendsData(data);
+    }, 300);
+  }, [data]);
+
+  if (!friendsData) {
     return (
-        <div>
-            <Banner friendsData={friendsData}></Banner>
-            <div className="container mx-auto p-4">
-                <div className="divider"></div>
-                <h3 className="font-semibold text-2xl mb-4">Your Friends</h3>
-            </div>
-            <FriendsList friendsData={friendsData}></FriendsList>
-        </div>
+      <div className="flex justify-center items-center h-[60vh]">
+        <span className="loading loading-dots loading-xl"></span>
+      </div>
     );
+  }
+  return (
+    <div>
+      <Banner friendsData={friendsData}></Banner>
+      <div className="container mx-auto p-4">
+        <div className="divider"></div>
+        <h3 className="font-semibold text-2xl mb-4">Your Friends</h3>
+      </div>
+      <FriendsList friendsData={friendsData}></FriendsList>
+    </div>
+  );
 };
 
 export default Homepage;
